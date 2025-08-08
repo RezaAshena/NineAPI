4f0d6e9eb22ee0f531b0d94440c56b560fcfbc3d
Reza Ashena <Ashena.reza@gmail.com>
2025-08-08 12:03:00 -0400

    Add BooksController and Book model for API
    Introduces a new `BooksController` in the `NineAPI.Controllers` namespace to handle HTTP requests related to books. A static list of `Book` objects is initialized within the controller.
    
    Also adds a `Book` class in the `NineAPI.Models` namespace, defining the structure of a book with properties for `Id`, `Title`, `Author`, and `YearPublished`, with `Title` and `Author` marked as required.

---
+++ NineAPI/Controllers/BooksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NineAPI.Models;
+
+namespace NineAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        static private List<Book> Books = new List<Book>
+        {
+            new Models.Book { Id = 1, Title = "1984", Author = "George Orwell", YearPublished = 1949 },
+            new Models.Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", YearPublished = 1960 },
+            new Models.Book { Id = 3, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", YearPublished = 1925 }
+        };
+    }
+}
